7cd64a6a464f4ec15e1ef93a3b4d2ce8a99a544e
Alberto de la Ossa <delaossa@mail.desy.de>
2014-04-02 21:12:02 +0200

    Fixed Mac OS compilation

--- config/envMac.sh
+++ config/envMac.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# MAC flag
-export PLATFORM=macosx
-
 # Setting up Root environment....
-export ROOTSYS=/usr/local/root
+export ROOTSYS=/Users/delaossa/local/root
 export DYLD_LIBRARY_PATH=$ROOTSYS/lib:$DYLD_LIBRARY_PATH
 export PATH=$ROOTSYS/bin:$PATH
 
 # HDF5: C++ interface
-export HDF5CPP=/usr/local/hdf5-c++
+export HDF5CPP=/usr/local/opt/hdf5
 export DYLD_LIBRARY_PATH=$HDF5CPP/lib:$DYLD_LIBRARY_PATH
 
 # HDF2ROOT environment:
-export HDF2ROOT=/Users/delaossa/plasma/hdf2root
+export HDF2ROOT=/Users/delaossa/plasma/software/hdf2root
 export DYLD_LIBRARY_PATH=$HDF2ROOT/lib:$DYLD_LIBRARY_PATH
 export PATH=$HDF2ROOT/bin:$PATH
 
